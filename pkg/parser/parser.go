7cefdf628d97e003086fde805a2cddcd909965bd
Thiago Henrique <fale@thiago.tech>
2022-10-30 18:59:37 -0300

    chore(parser): create package structure

---
+++ pkg/parser/parser.go
@@ -0,0 +1,29 @@
+// parser is the package containing tools
+// to extract and standardize data from banks
+package parser
+
+import (
+	"time"
+)
+
+// BankFilePath represents the path to
+// a document provided by the bank
+type BankFilePath string
+
+// Entry represents a financial
+// entry extracted from the account
+type Entry struct {
+	ID     string
+	Date   time.Time
+	Desc   string
+	Amount int
+}
+
+// Interface that must be implemented
+// by any data extractor
+type Interface interface {
+
+	// GetEntries is where the entries
+	// is extracted from the BankFilePath
+	GetEntries(BankFilePath) []Entry
+}
