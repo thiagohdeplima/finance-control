1d410fdc08ffc96109f154495778dd0de6c3b3ae
Thiago Henrique <fale@thiago.tech>
2022-12-31 18:24:55 -0300

    chore: create new error type

--- pkg/parser/error.go
+++ pkg/parser/error.go
@@ -8,6 +8,15 @@ type FileNotFoundError struct {
 	Path BankFilePath
 }
 
+type InvalidBankFileError struct {
+	Path BankFilePath
+	Err  string
+}
+
 func (e *FileNotFoundError) Error() string {
 	return fmt.Sprintf("the file %s doesn't exists", e.Path)
+}
+
+func (e *InvalidBankFileError) Error() string {
+	return fmt.Sprintf("the file %s is invalid: `%s`", e.Path, e.Err)
 }
